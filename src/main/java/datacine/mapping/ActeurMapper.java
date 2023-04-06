31af639a8a13727c97dce6712a3ab6e5d3fa649f
test <rem>
2023-04-06 07:21:14 +0000

    premiere version du front

--- src/main/java/datacine/mapping/ActeurMapper.java
+++ src/main/java/datacine/mapping/ActeurMapper.java
@@ -2,10 +2,8 @@ package datacine.mapping;
 
 import datacine.domain.Acteur;
 import datacine.dto.ActeurDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(uses = {FilmMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+
 public interface ActeurMapper {
 
     ActeurDto acteurToDto(Acteur acteur);
--- src/main/java/datacine/mapping/RealisateurMapper.java
+++ src/main/java/datacine/mapping/RealisateurMapper.java
@@ -2,10 +2,8 @@ package datacine.mapping;
 
 import datacine.domain.Realisateur;
 import datacine.dto.RealisateurDto;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(uses = {FilmMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+
 public interface RealisateurMapper {
 
     RealisateurDto realisateurToDto(Realisateur realisateur);
