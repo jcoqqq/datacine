d820792fe2d5e64dbaaefc50ac10be7b80572ce6
test <rem>
2023-04-06 07:12:47 +0000

    premiere version du front

--- src/main/java/datacine/controller/FrontController.java
+++ src/main/java/datacine/controller/FrontController.java
@@ -1,21 +1,49 @@
 package datacine.controller;
 
+import datacine.service.Datafront;
 import datacine.service.RedirectPage;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.*;
 
 @RestController
 @EnableWebMvc
 @RequestMapping(value = "/")
 public class FrontController {
-    private RedirectPage redirect;
+    private RedirectPage redirect = new RedirectPage();
+    @GetMapping("/")
+    public ModelAndView index(@PathVariable(required = false,name="page") String page, Model model) {
 
-    @GetMapping(value = "/")
-    public ModelAndView hello(@RequestParam(required = false) String home) {
+        Datafront data = new Datafront();
+       data.film();
+        System.out.println(data.gethtml());
+        model.addAttribute("data", data);
+       // model.addAttribute("data", new Datafront()); // Ajouter un objet Data à votre modèle et vue
+
         ModelAndView modelAndView = new ModelAndView();
-        String page=redirect.Page(home);
-        modelAndView.setViewName(page);
+        modelAndView.setViewName("index");
         return modelAndView;
+    }
+
+    @RequestMapping(value = "/{page}")
+    public ModelAndView hello(@PathVariable(required = false,name="page") String page) {
+        ModelAndView modelAndView = new ModelAndView();
+        String verifpage=redirect.Page(page);
+        modelAndView.setViewName(verifpage);
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/css/{fichier}")
+    public String css(@PathVariable(required = false,name="fichier") String fichier)  {
+            String contenu=redirect.chemincss(fichier);
+            return contenu;
+    }
+    @RequestMapping(value = "/js/{fichier}")
+    public String js(@PathVariable(required = false,name="fichier") String fichier)  {
+        String contenu=redirect.cheminjs(fichier);
+        return contenu;
     }
 }
--- src/main/java/datacine/rest/DataBaseRest.java
+++ src/main/java/datacine/rest/DataBaseRest.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @RequestMapping(value = "/v1")
+@EnableWebMvc
 public interface DataBaseRest {
 
     @PostMapping(value = "/add/acteur")
