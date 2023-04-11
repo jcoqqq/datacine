411be1fc0707f858a8fe57f785eef521baefeca0
jcoqqq <j.coquin76@gmail.com>
2023-04-11 11:32:16 +0200

    minis corrections

--- src/main/java/datacine/controller/FrontController.java
+++ src/main/java/datacine/controller/FrontController.java
@@ -7,20 +7,24 @@ import jakarta.activation.FileTypeMap;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 
 @RestController
 @EnableWebMvc
 @RequestMapping(value = "/")
 public class FrontController {
-    private RedirectPage redirect = new RedirectPage();
+
+    private final RedirectPage redirect = new RedirectPage();
+
     @GetMapping("/")
     public ModelAndView index(@PathVariable(required = false,name="page") String page, Model model) {
 
@@ -38,10 +42,11 @@ public class FrontController {
     @RequestMapping(value = "/{page}")
     public ModelAndView hello(@PathVariable(required = false,name="page") String page) {
         ModelAndView modelAndView = new ModelAndView();
-        String verifpage=redirect.Page(page);
+        String verifpage = redirect.Page(page);
         modelAndView.setViewName(verifpage);
         return modelAndView;
     }
+
     @RequestMapping(value = "/search/{search}")
     public ModelAndView search(@PathVariable(required = false,name="search") String search, Model model) {
 
@@ -58,9 +63,9 @@ public class FrontController {
 
     @RequestMapping(value = "/css/{fichier}")
     public String css(@PathVariable(required = false,name="fichier") String fichier)  {
-            String contenu=redirect.chemincss(fichier);
-            return contenu;
+        return redirect.chemincss(fichier);
     }
+
     @RequestMapping("/film/{id}")
     public ModelAndView film(@PathVariable(required = false,name="id") String id, Model model) {
         DataFrontFilmRea dataFrontFilmRea = new DataFrontFilmRea();
@@ -71,26 +76,29 @@ public class FrontController {
         modelAndView.setViewName("filmrea");
         return modelAndView;
     }
+
     @RequestMapping(value = "/js/{fichier}")
     public String js(@PathVariable(required = false,name="fichier") String fichier)  {
-        String contenu=redirect.cheminjs(fichier);
-        return contenu;
+        return redirect.cheminjs(fichier);
     }
+
     @RequestMapping(value = "/image/film/{id}")
     public ResponseEntity<byte[]> getimage(@PathVariable(name="id") String id) throws IOException{
-        String fichier=redirect.getnameimagefile(id);
+        String fichier = redirect.getnameimagefile(id);
         File img = new File("src/main/resources/templates/images/films/"+fichier);
         return ResponseEntity.ok().contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img))).body(Files.readAllBytes(img.toPath()));
     }
+
     @RequestMapping(value = "/image/realisateur/{id}")
     public ResponseEntity<byte[]> getrealisateur(@PathVariable(name="id") String id) throws IOException{
-        String fichier=redirect.getnameimageacteur(id);
+        String fichier = redirect.getnameimageacteur(id);
         File img = new File("src/main/resources/templates/images/realisateurs/"+fichier);
         return ResponseEntity.ok().contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img))).body(Files.readAllBytes(img.toPath()));
     }
+
     @RequestMapping(value = "/image/acteur/{id}")
     public ResponseEntity<byte[]> getacteur(@PathVariable(name="id") String id) throws IOException{
-        String fichier=redirect.getnameimagerealisteaur(id);
+        String fichier = redirect.getnameimagerealisteaur(id);
         File img = new File("src/main/resources/templates/images/acteurs/"+fichier);
         return ResponseEntity.ok().contentType(MediaType.valueOf(FileTypeMap.getDefaultFileTypeMap().getContentType(img))).body(Files.readAllBytes(img.toPath()));
     }
